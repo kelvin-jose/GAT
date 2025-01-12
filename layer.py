41dd64260ccdfe98977f6a6cbe169766be2dd34a
Kelvin <kelvinkonoor@gmail.com>
2025-01-12 22:13:34 +0530

    important params

--- layer.py
+++ layer.py
@@ -1,8 +1,18 @@
 import torch
 
 class GATLayer(torch.nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, fin, fout, num_heads, concat=True):
+        super().__init__()
+        self.num_heads = num_heads
+        self.fout = fout
+        self.W = torch.nn.Linear(fin, num_heads * fout, bias=False)
+        self.source_scorer = torch.nn.Parameter(torch.Tensor(num_heads, 1, fout))
+        self.target_scorer = torch.nn.Parameter(torch.Tensor(num_heads, 1, fout))
+        self.l_relu = torch.nn.ELU(0.2)
+        self.concat = concat
+        self.drop1 = torch.nn.Dropout(0.6)
+        self.drop2 = torch.nn.Dropout(0.6)
+
 
     def forward(self):
         pass
