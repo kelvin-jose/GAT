558394eebf898f5dc41c4b106964eb35cc8221ad
Kelvin <kelvinkonoor@gmail.com>
2025-01-12 22:02:26 +0530

    adding a sampling function

--- gat.py
+++ gat.py
@@ -15,3 +15,18 @@ node_labels = np.load(f'{CORA_TARGET_LOCATION}/{LABELS}', allow_pickle=True)
 
 label_encoder = preprocessing.LabelEncoder()
 labels2idx = torch.tensor(label_encoder.fit_transform(node_labels), dtype = torch.long)
+
+seed = 52
+np.random.seed(seed)
+
+def random_stratified_train_indices(items, sample_size=20):
+    unique_items = np.unique(items)
+    indices = []
+    for item in unique_items:
+        item_indices = np.where(items == item)[0]
+        if len(item_indices) > sample_size:
+            sample_indices = np.random.choice(item_indices, sample_size, replace=False)
+        else:
+            sample_indices = item_indices
+        indices.extend(sample_indices)
+    return indices
