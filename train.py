51ae2e3aafe84bf7f87bbd2240657c5b2c54515d
Kelvin <kelvinkonoor@gmail.com>
2025-01-12 22:27:01 +0530

    renamed gat to train

---
+++ train.py
@@ -0,0 +1,57 @@
+import torch
+import random
+import numpy as np
+from sklearn import preprocessing 
+from matplotlib import pyplot as plt
+
+from layer import GATLayer
+
+CORA_TARGET_LOCATION = '../datasets/cora_preprocessed'
+EDGE_INFO = 'cora_edge_index.npy'
+FEATURES = 'cora_features.npy'
+LABELS = 'cora_labels.npy'
+
+edge_index = np.load(f'{CORA_TARGET_LOCATION}/{EDGE_INFO}')
+node_features = np.load(f'{CORA_TARGET_LOCATION}/{FEATURES}')
+node_labels = np.load(f'{CORA_TARGET_LOCATION}/{LABELS}', allow_pickle=True)
+
+label_encoder = preprocessing.LabelEncoder()
+labels2idx = torch.tensor(label_encoder.fit_transform(node_labels), dtype = torch.long)
+
+seed = 52
+np.random.seed(seed)
+
+def random_stratified_train_indices(items, sample_size=20):
+    unique_items = np.unique(items)
+    indices = []
+    for item in unique_items:
+        item_indices = np.where(items == item)[0]
+        if len(item_indices) > sample_size:
+            sample_indices = np.random.choice(item_indices, sample_size, replace=False)
+        else:
+            sample_indices = item_indices
+        indices.extend(sample_indices)
+    return indices
+
+train_indices = random_stratified_train_indices(node_labels)
+other_indices = np.setdiff1d(range(len(node_labels)), train_indices)
+val_indices = np.random.choice(other_indices, 500, replace=False)
+other_indices = np.setdiff1d(range(len(other_indices)), val_indices)
+test_indices = np.random.choice(other_indices, 1000, replace=False)
+
+print('train data size: ', len(train_indices))
+print('valid data size: ', len(val_indices))
+print('test data size: ', len(test_indices))
+
+train_config = {
+    'num_layers': 2,
+    'fin': [1433, 64],
+    'num_heads': [8, 1],
+    'fout': [8, 7],
+    'concat': [True, False],
+    'num_epochs': 1000,
+    'lr': 0.005,
+    'weight_decay': 0.0005
+}
+
+
