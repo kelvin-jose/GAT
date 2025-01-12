5a65b153035174dd772307d39af95ae81a44f2dd
Kelvin <kelvinkonoor@gmail.com>
2025-01-12 21:56:05 +0530

    encoding node labels

--- gat.py
+++ gat.py
@@ -8,3 +8,10 @@ CORA_TARGET_LOCATION = '../datasets/cora_preprocessed'
 EDGE_INFO = 'cora_edge_index.npy'
 FEATURES = 'cora_features.npy'
 LABELS = 'cora_labels.npy'
+
+edge_index = np.load(f'{CORA_TARGET_LOCATION}/{EDGE_INFO}')
+node_features = np.load(f'{CORA_TARGET_LOCATION}/{FEATURES}')
+node_labels = np.load(f'{CORA_TARGET_LOCATION}/{LABELS}', allow_pickle=True)
+
+label_encoder = preprocessing.LabelEncoder()
+labels2idx = torch.tensor(label_encoder.fit_transform(node_labels), dtype = torch.long)
