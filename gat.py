1d774b341b762ceaeaafa480e395d107c9f9517e
Kelvin <kelvinkonoor@gmail.com>
2025-01-12 21:54:53 +0530

    defining some constants, npy files come from the EDA notebook

--- gat.py
+++ gat.py
@@ -3,3 +3,8 @@ import random
 import numpy as np
 from sklearn import preprocessing 
 from matplotlib import pyplot as plt
+
+CORA_TARGET_LOCATION = '../datasets/cora_preprocessed'
+EDGE_INFO = 'cora_edge_index.npy'
+FEATURES = 'cora_features.npy'
+LABELS = 'cora_labels.npy'
