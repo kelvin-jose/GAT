7618ab2ed5fcda62178b1e2d6204e5820cd2f765
Kelvin <kelvinkonoor@gmail.com>
2025-01-12 22:18:12 +0530

    this function pulls source and target features according to the edge list

--- layer.py
+++ layer.py
@@ -14,9 +14,20 @@ class GATLayer(torch.nn.Module):
         self.drop2 = torch.nn.Dropout(0.6)
 
     def forward(self):
-        pass
+        x = self.drop1(x)
+        x = self.W(x).view(self.num_heads, -1, self.fout) # [num_heads, nodes, fout]
+        source_scores = (x * self.source_scorer).sum(-1)
+        target_scores = (x * self.target_scorer).sum(-1)
 
     def init_params(self):
         torch.nn.init.xavier_uniform_(self.W.weight, gain=1.414)
         torch.nn.init.xavier_uniform_(self.source_scorer)
         torch.nn.init.xavier_uniform_(self.target_scorer)
+
+    
+    def select_source_target_feats(self, source_scores, target_scores, features, edge_index):
+        selected_scores = torch.zeros(self.num_heads, edge_index.shape[0], edge_index.shape[1])
+        selected_features = torch.index_select(features, 1, torch.tensor(edge_index[:, 0]))
+        selected_scores[:, :, 0] = torch.index_select(source_scores, 1, torch.tensor(edge_index[:, 0]))
+        selected_scores[:, :, 1] = torch.index_select(target_scores, 1, torch.tensor(edge_index[:, 1]))
+        return selected_features, selected_scores
